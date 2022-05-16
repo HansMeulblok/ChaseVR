c4c8ff545eacdb4919ce0d6a5cd7e102a1d70fd2
Thomas Haagsma <t_haagsma1998@hotmail.com>
2022-05-16 11:52:07 +0200

    Refactored ClotherTags script

--- ChaseVR/Assets/Scripts/ClothesTags.cs
+++ ChaseVR/Assets/Scripts/ClothesTags.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class ClothesTags : MonoBehaviour
 {
-    public GameObject TorsoHitbox;
-    public GameObject BenenHitbox;
-    public GameObject SchoenenHitbox;
-    public GameObject Mannequin;
+    public Transform TorsoPosition;
+    public Transform BenenPosition;
+    public Transform SchoenenPosition;
+    //public GameObject Mannequin;
 
     private GameObject _torsoKleding;
     private GameObject _benenKleding;
@@ -16,56 +16,45 @@ public class ClothesTags : MonoBehaviour
 
     private void OnTriggerEnter(Collider Clothes)
     {
-        Debug.Log($"({Clothes.name}, enter) parent: {Clothes.transform.parent?.name ?? "none"}");
+        //Debug.Log($"({Clothes.name}, enter) parent: {Clothes.transform.parent?.name ?? "none"}");
         switch (Clothes.gameObject.tag)
         {
             case "Torso":
 
-
-                if (_torsoKleding == null && Clothes.tag == "Torso")
+                if (_torsoKleding == null)
                 {
                     _torsoKleding = Clothes.gameObject;
-                    Clothes.transform.position = TorsoHitbox.transform.position;
-                    Clothes.transform.rotation = TorsoHitbox.transform.rotation;
-                    Clothes.attachedRigidbody.velocity = new Vector3(0, 0, 0);
-                    Clothes.attachedRigidbody.angularVelocity = new Vector3(0, 0, 0);
-                    Clothes.gameObject.transform.SetParent(TorsoHitbox.transform.parent.parent, true);
-                    
+                    SetCorrectClothesTransform(Clothes, TorsoPosition);
                     break;
                 }
+
                 break;
              
 
             case "Benen":
                 
-                if (_benenKleding == null && Clothes.tag == "Benen")
+                if (_benenKleding == null)
                 {
                     _benenKleding = Clothes.gameObject;
-                    Clothes.transform.position = BenenHitbox.transform.position;
-                    Clothes.transform.rotation = BenenHitbox.transform.rotation;
-                    Clothes.attachedRigidbody.velocity = new Vector3(0, 0, 0);
-                    Clothes.attachedRigidbody.angularVelocity = new Vector3(0, 0, 0);
-                    Clothes.gameObject.transform.SetParent(BenenHitbox.transform.parent.parent, true);
+                    SetCorrectClothesTransform(Clothes, BenenPosition);
                     break;
                 }
-                else
-                    break;
+
+                break;
 
             case "Schoenen":
-                if (_schoenenKleding == null && Clothes.tag == "Schoenen")
+                if (_schoenenKleding == null)
                 {
                     _schoenenKleding = Clothes.gameObject;
-                    Clothes.transform.position = SchoenenHitbox.transform.position;
-                    Clothes.transform.rotation = SchoenenHitbox.transform.rotation;
-                    Clothes.attachedRigidbody.velocity = new Vector3(0, 0, 0);
-                    Clothes.attachedRigidbody.angularVelocity = new Vector3(0, 0, 0);
-                    Clothes.gameObject.transform.SetParent(SchoenenHitbox.transform.parent.parent, true);
+                    SetCorrectClothesTransform(Clothes, SchoenenPosition);
                     break;
                 }
-                else
-                    break;
+                
+                break;
 
 
+            default:
+                break;
         }
     }
 
@@ -82,20 +71,25 @@ public class ClothesTags : MonoBehaviour
                 break;
 
             case "Benen":
-                Debug.Log("Benen");
                 if (_benenKleding == Clothes.gameObject)
                     _benenKleding = null;
                 break;
 
             case "Schoenen":
-                Debug.Log("Schoenen");
                 if (_schoenenKleding == Clothes.gameObject)
                     _schoenenKleding = null;
                 break;
 
 
         }
-
     }
 
+    public void SetCorrectClothesTransform(Collider clothes, Transform clothingTransform)
+    {
+        clothes.transform.position = clothingTransform.position;
+        clothes.transform.rotation = clothingTransform.rotation;
+        clothes.attachedRigidbody.velocity = Vector3.zero;
+        clothes.attachedRigidbody.angularVelocity = Vector3.zero;
+        clothes.gameObject.transform.SetParent(clothingTransform.parent.parent, true);
+    }
 }

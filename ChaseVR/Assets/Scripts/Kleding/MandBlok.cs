bef9b7e031a090c9151d89260bd3e0d49588418b
Thomas Haagsma <t_haagsma1998@hotmail.com>
2022-05-18 16:41:17 +0200

    Added UI panel that shows item ids of items

--- ChaseVR/Assets/Scripts/Kleding/MandBlok.cs
+++ ChaseVR/Assets/Scripts/Kleding/MandBlok.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using TMPro;
 
 //[RequireComponent(typeof(Rigidbody))]
 public class MandBlok : MonoBehaviour
 {
     [SerializeField]
     private List<int> artikelenInMand = new List<int>();
+
+    public GameObject uiContent;
+    public GameObject mandPanelPrefab;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -37,6 +41,10 @@ public class MandBlok : MonoBehaviour
         {
             artikelenInMand.Add(clothing.transform.parent.GetComponent<KledingStuk>().artikelNummer);
 
+            GameObject child = Instantiate(mandPanelPrefab, uiContent.transform);
+            child.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = clothing.name;
+
+
             clothing.GetComponent<Rigidbody>().useGravity = true;
             clothing.GetComponent<BoxCollider>().isTrigger = false;
             clothing.GetComponent<XRGrabInteractable>().enabled = true;
@@ -46,5 +54,13 @@ public class MandBlok : MonoBehaviour
     public void RemoveClothingFromMand(GameObject clothing)
     {
         artikelenInMand.Remove(int.Parse(clothing.gameObject.name));
+
+        foreach (Transform child in uiContent.transform)
+        {
+            if (child.GetComponentInChildren<TextMeshProUGUI>().text == clothing.name)
+            {
+                Destroy(child.gameObject);
+            }
+        }
     }
 }
